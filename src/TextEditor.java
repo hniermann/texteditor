835e228e20c0b43c3b359230b1a4a21268ea69e8
Henry Niermann <henryniermann4@gmail.com>
2020-04-08 18:43:02 -0400

    Add look and feel

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,3 +1,5 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public final class TextEditor extends View {
 
@@ -6,6 +8,24 @@ public final class TextEditor extends View {
     }
 
     public static void main(String args[]) {
+
+        // Set System L&F
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         Model model = new Model();
         View view = new View();
         Controller controller = new Controller(view, model);
